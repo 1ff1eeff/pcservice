79c45560eb9b4e54a4083bd4894ef68c56c87b49
1ff1eeff <1ff1eeff@gmail.com>
2017-02-21 10:57:24 +0300

    Add files via upload

--- site/js/scripts.js
+++ site/js/scripts.js
@@ -1,14 +1,31 @@
-  $(document).ready(function(){
-    $("#sticker").sticky({topSpacing:0});
-    $("#sticker").sticky({bottomSpacing:0});
-    $('#sticker').on('sticky-start', function() {
-        document.getElementById('mini-logo').style.visibility = 'visible'; 
-        document.getElementById('mini-caption').style.visibility = 'visible';
-        document.getElementById('mini-phone').style.visibility = 'visible'
+$(document).ready(function(){
+  $("#sticker").sticky({topSpacing:0});
+  $("#sticker").sticky({bottomSpacing:0});
+  $('#sticker').on('sticky-start', function() {
+      document.getElementById('mini-logo').style.visibility = 'visible'; 
+      document.getElementById('mini-caption').style.visibility = 'visible';
+      document.getElementById('mini-phone').style.visibility = 'visible'
     } );
-    $('#sticker').on('sticky-end', function() {
-        document.getElementById('mini-logo').style.visibility = 'hidden'; 
-        document.getElementById('mini-caption').style.visibility = 'hidden';
-        document.getElementById('mini-phone').style.visibility = 'hidden';
-    } );
+
+
+$('#sticker').on('sticky-end', function() {
+    document.getElementById('mini-logo').style.visibility = 'hidden'; 
+    document.getElementById('mini-caption').style.visibility = 'hidden';
+    document.getElementById('mini-phone').style.visibility = 'hidden';
+} );
+});
+
+
+
+function initMap() {
+  var myLatLng = {lat: 54.191236, lng: 37.616218};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    zoom: 15
   });
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Компьютерная помощь'
+  });  
+}
